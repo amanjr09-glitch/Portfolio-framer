2931578a17b96bd93188431fe20734d8189686c7
Sasuke <atsyt09@gmail.com>
2023-09-02 23:27:34 +0530

    starter

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,31 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/Vxibhxv_?t=BJKZ_R-m8tsywk6hIAmgMg&s=09',
+    icon: 'twitter',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/amanjr09-glitch',
+    icon: 'github',
+  },
+];
